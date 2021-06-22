d96dbe06c8349120d1d7fb2c0004326b59f6dfe9
PanovPavel <49455695+PanovPavel@users.noreply.github.com>
2021-06-22 08:23:58 +0300

    delete logs

--- src/main/java/ClientDemoSocket/ControllerClient.java
+++ src/main/java/ClientDemoSocket/ControllerClient.java
@@ -1,30 +1,5 @@
 package ClientDemoSocket;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import java.awt.*;
-import java.io.*;
-import java.net.Socket;
-import javafx.scene.control.TextField;
-public class ControllerClient {
-    @FXML
-    TextField textFieldCity, textFieldTemp;
 
-    public void send(ActionEvent actionEvent) {
-        Client client = new Client(4001);
-        try {
-            /**
-             * Отправить название города на сервер
-             */
-            client.sendCityOnServer(textFieldCity.getText());
-            /**
-             * Получить ответ сервера
-             */
-            textFieldTemp.setText("Температура = " + client.receiveAnswerWithServer());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        finally {
-           client.closeConnect();
-        }
-    }
+public class ControllerClient {
+
 }
--- src/main/java/ServerDemoSocket/ControllerServer.java
+++ src/main/java/ServerDemoSocket/ControllerServer.java
@@ -3,23 +3,5 @@ package ServerDemoSocket;
 import javafx.event.ActionEvent;
 
 public class ControllerServer {
-    MeteoServer meteoServer;
-    Thread thread;
-    public void stopButton(ActionEvent actionEvent) {
-        thread.stop();
-        meteoServer.stop();
-    }
 
-    public void startButton(ActionEvent actionEvent) {
-        thread = new Thread(runnableServer);
-        thread.start();
-    }
-    Runnable runnableServer = new Runnable() {
-        public void run() {
-            meteoServer = new MeteoServer(4001);
-            while (true) {
-                meteoServer.run();
-            }
-        }
-    };
 }
