53ea1fefeb4ef3d42f8627559ebc583066b45600
PanovPavel <49455695+PanovPavel@users.noreply.github.com>
2021-06-23 05:27:53 +0300

    я

--- src/main/java/ClientDemoSocket/ControllerClient.java
+++ src/main/java/ClientDemoSocket/ControllerClient.java
@@ -1,5 +1,53 @@
 package ClientDemoSocket;
 
-public class ControllerClient {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import java.awt.*;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class ControllerClient {
+    private static Socket clientSocket;
+    private static BufferedReader reader;
+    private static BufferedReader in;
+    private static BufferedWriter out;
+    @FXML
+    TextField textFieldCity;
+    @FXML
+    TextField textFieldTemp;
+    public void initialize(){
+        try {
+            clientSocket = new Socket("localhost", 4001);
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void send(ActionEvent actionEvent) {
+        Thread thread = new Thread(runnableClient);
+        thread.start();
+    }
+    Runnable runnableClient = new Runnable() {
+        public void run() {
+            try {
+                System.out.println("0");
+                out.write(textFieldCity.getText() + "\n");
+                System.out.println("1");
+                out.flush();
+                System.out.println("2");
+                String serverWord = in.readLine();
+                System.out.println("3");
+                textFieldTemp.setText(serverWord);
+                System.out.println("4");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    };
 }
--- src/main/java/ServerDemoSocket/ControllerServer.java
+++ src/main/java/ServerDemoSocket/ControllerServer.java
@@ -2,6 +2,59 @@ package ServerDemoSocket;
 
 import javafx.event.ActionEvent;
 
-public class ControllerServer {
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
 
+public class ControllerServer {
+    HashMap<String, String> cityTemp = new HashMap<String, String>();
+    private static Socket clientSocket;
+    private static ServerSocket server;
+    private static BufferedReader in;
+    private static BufferedWriter out;
+
+    public void initialize(){
+        cityTemp.put("Воронеж", "28");
+        cityTemp.put("Липецк", "30");
+        cityTemp.put("Москва", "32");
+        cityTemp.put("Киев", "34");
+    }
+
+
+    public void startButton(ActionEvent actionEvent) {
+       Thread thread = new Thread(runnableServer);
+       thread.start();
+    }
+
+    public void stopButton(ActionEvent actionEvent) {
+        try {
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    Runnable runnableServer = new Runnable() {
+        public void run() {
+            try {
+                server = new ServerSocket(4001);
+                clientSocket = server.accept();
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                System.out.println("0");
+                String CityNameIn = in.readLine();
+                System.out.println("1");
+                out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+                System.out.println("2");
+                out.write(cityTemp.get(CityNameIn));
+                System.out.println("3");
+                out.flush();
+                System.out.println("4");
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    };
 }
